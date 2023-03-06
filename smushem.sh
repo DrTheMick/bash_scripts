3434da98ec11342cefaaae3b26f884f0fd809c89
Michael Martynowycz <michael.martynowycz@gmail.com>
2023-03-06 12:34:56 -0800

    Update smushem.sh
    Fixed to take both input and output dirs as command line parameters.

--- smushem.sh
+++ smushem.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
 #Bash script to sum all eer or mrc files in a given directory. The output is a tif file with the same basename.
-#This takes the directory as a command line parameter. 
+#This takes two inputs. The input directory and the output directories as command line parameters.
 #This requires the alpha version of mrc2smv
 
-# Check that a directory argument was provided
-if [ -z "$1" ]; then
-  echo "Usage: $0 DIRECTORY"
+# Check that two arguments were provided
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 INPUT_DIR OUTPUT_DIR"
   exit 1
 fi
 
-# Set the directory containing the EER/MRC files
-directory="$1"
+# Set the input and output directories
+input_dir="$1"
+output_dir="$2"
 
-# Check that the directory exists
-if [ ! -d "$directory" ]; then
-  echo "Error: Directory $directory not found"
+# Check that the input directory exists
+if [ ! -d "$input_dir" ]; then
+  echo "Error: Input directory $input_dir not found"
   exit 1
+fi
+
+# Check that the output directory exists (or create it if it doesn't)
+if [ ! -d "$output_dir" ]; then
+  mkdir -p "$output_dir"
 fi
 
 # Loop over all files ending in .eer or .mrc
-for f in "$directory"/*.{eer,mrc}; do
+for f in "$input_dir"/*.{eer,mrc}; do
   # Check that the file exists
   if [ ! -f "$f" ]; then
     continue
@@ -30,5 +36,6 @@ for f in "$directory"/*.{eer,mrc}; do
   prefix=$(basename "$prefix" .mrc)
 
   # Run the mrc2smv command with the appropriate input flags
-  mrc2smv -A999999 -f -o"$prefix".tif "$f"
+  mrc2smv -A999999 -f -o"$output_dir/$prefix".tif "$f"
 done
+
